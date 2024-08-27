d7842e9650658815c4654a1e2ac839aaf7963b04
david gomes <davidgomesk8@gmail.com>
2024-08-27 13:29:07 +0200

    seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,34 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Property.destroy_all
+
+Property.create!({
+  name: "Sample Property",
+  description: "This is a sample property",
+  headline: "Sample Property",
+  address_1: "1234 Sample St",
+  address_2: "Unit 1",
+  city: "Sample City",
+  state: "CA",
+  country: "US",
+})
+
+Property.create!({
+  name: "Sample Property 2",
+  description: "This is a sample property 2",
+  headline: "Sample Property 2",
+  address_1: "50 Sample St",
+  address_2: "Unit 1",
+  city: "Brooklyn",
+  state: "NY",
+  country: "US",
+})
+
+Property.create!({
+  name: "Sample Property 3",
+  description: "This is a sample property 3",
+  headline: "Sample Property 3",
+  address_1: "12 Sample St",
+  address_2: "Unit 1",
+  city: "Boston",
+  state: "MA",
+  country: "US",
+})
