a166aa3f7cf6b4f947ce1e509193d18e0b00e6fb
david gomes <davidgomesk8@gmail.com>
2024-08-30 12:35:45 +0200

    if user not logged in they can't access wishlist

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -2,6 +2,15 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   updateWishListStatus() {
+    // Get status if user is logged in or not
+    // If user is not logged in, redirect to login page
+    // else continue with the below code
+
+    const isUserLoggedIn = this.element.dataset.userLoggedIn;
+    if (isUserLoggedIn === "false") {
+      document.querySelector(".js-login").click();
+      return;
+    }
     if (this.element.dataset.status === "false") {
       this.element.classList.remove("fill-rgba(0,0,0,0.5)");
       this.element.classList.add("fill-primary");
