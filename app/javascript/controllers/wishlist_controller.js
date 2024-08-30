18c1ef1af12b8a1187fa1afb44d011dbfde740f3
david gomes <davidgomesk8@gmail.com>
2024-08-30 12:13:18 +0200

    toggled the heart svg in image swipe

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  connect() {
-    console.log("Wishlist connected!");
+  updateWishListStatus() {
+    if (this.element.dataset.status === "false") {
+      this.element.classList.remove("fill-rgba(0,0,0,0.5)");
+      this.element.classList.add("fill-primary");
+      this.element.dataset.status = "true";
+    } else {
+      this.element.classList.add("fill-rgba(0,0,0,0.5)");
+      this.element.classList.remove("fill-primary");
+      this.element.dataset.status = "false";
+    }
   }
 }
