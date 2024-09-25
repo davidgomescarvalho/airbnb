872841945e1970a8b5caf1f7cca70caad41b1763
david gomes <davidgomesk8@gmail.com>
2024-09-25 22:45:22 +0200

    changed overall rating in properties controller to default to 0

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,10 +1,10 @@
 class PropertiesController < ApplicationController
 
   def show
-    @property = Property.find(params[:id])
+    @property = Property.includes(:reviews).find(params[:id])
 
     # overal rating converted to integer for each review
     @overall_rating_counts = @property.reviews.group('ROUND(final_rating)').count.transform_keys(&:to_i)
-
+    @overall_rating_counts.default = 0
   end
 end
