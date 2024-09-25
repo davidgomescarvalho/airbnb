be1a5bcec9dee25388050334cf4f45f7cb5d7571
david gomes <davidgomesk8@gmail.com>
2024-09-25 21:34:10 +0200

    added overall rating in controller and now implementing in show

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,5 +2,9 @@ class PropertiesController < ApplicationController
 
   def show
     @property = Property.find(params[:id])
+
+    # overal rating converted to integer for each review
+    @overall_rating_counts = @property.reviews.group('ROUND(final_rating)').count.transform_keys(&:to_i)
+
   end
 end
