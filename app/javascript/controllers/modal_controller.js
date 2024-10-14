e1ad53c1396e4bb04039cdda2e04852d78418197
david gomes <davidgomesk8@gmail.com>
2024-10-14 23:51:26 +0200

    added amenities controller in js and switched to toggle in data crontroller to all the data controllers for modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,14 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
-import { enter, leave, toggle } from "el-transition";
+import { toggle } from "el-transition";
 
 export default class extends Controller {
   connect() {}
 
-  show() {
-    enter(document.getElementById("modal-wrapper"));
-  }
-
-  hide() {
-    leave(document.getElementById("modal-wrapper"));
+  toggleModal() {
+    toggle(document.getElementById("modal-wrapper"));
   }
 }
