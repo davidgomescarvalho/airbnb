ae374248c3c61424e626e2039ee2176cecffcd0e
david gomes <davidgomesk8@gmail.com>
2024-11-01 12:07:46 +0100

    fixed date picker and now working as espected

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -15,17 +15,19 @@ export default class extends Controller {
     flatpickr(this.checkinTarget, {
       minDate: new Date().fp_incr(1),
       onChange: (selectedDates, dateStr, instance) => {
-        this.triggerCheckoutDatePicker();
+        this.triggerCheckoutDatePicker(selectedDates);
       },
     });
 
     this.updateDetails();
   }
 
-  triggerCheckoutDatePicker() {
+  triggerCheckoutDatePicker(selectedDates) {
     flatpickr(this.checkoutTarget, {
-      minDate: new Date().fp_incr(1),
-      onChange: (selectedDates, dateStr, instance) => {},
+      minDate: new Date(selectedDates).fp_incr(1),
+      onChange: (selectedDates, dateStr, instance) => {
+        this.updateDetails();
+      },
     });
 
     this.checkoutTarget.click();
