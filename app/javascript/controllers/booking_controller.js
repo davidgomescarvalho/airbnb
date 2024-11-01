879c5ad4bf813fad1a6f65d53f2728bc58cd7cb4
david gomes <davidgomesk8@gmail.com>
2024-11-01 13:31:41 +0100

    changed format for placeholder in checkin and checkout

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -18,6 +18,8 @@ export default class extends Controller {
         this.triggerCheckoutDatePicker(selectedDates);
       },
     });
+
+    this.updateDetails();
   }
 
   triggerCheckoutDatePicker(selectedDates) {
