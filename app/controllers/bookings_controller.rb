75e12807eec69e118205aeb85057c2ded1683151
david gomes <davidgomesk8@gmail.com>
2024-11-04 20:45:26 +0100

    ading information in booking show page, done with dates, and name

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,27 @@
 class BookingsController < ApplicationController
   def new
+    @property = Property.find(params[:property_id])
+    @checkin_date = params[:checkin_date]
+    @checkout_date = params[:checkout_date]
 
+    # Number of nights
+    @total_nights = number_of_nights
+
+    # Base Fare
+    @base_fare = @property.price
+    # Service Fee
+    # Total Price
+  end
+
+  private
+
+  def number_of_nights
+    checkin_date = Date.parse(params[:checkin_date])
+    checkout_date = Date.parse(params[:checkout_date])
+    return (checkout_date - checkin_date).to_i
+  end
+
+  def booking_params
+    params.permit(:property_id, :checkin_date, :checkout_date)
   end
 end
