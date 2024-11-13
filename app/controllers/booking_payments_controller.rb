8d61688846677ee65e5dc411276cd2936f31d132
david gomes <davidgomesk8@gmail.com>
2024-11-13 19:53:37 +0100

    added private booking params in controller

--- app/controllers/booking_payments_controller.rb
+++ app/controllers/booking_payments_controller.rb
@@ -1,5 +1,20 @@
 class BookingPaymentsController < ApplicationController
   def create
-    raise
+
+  end
+
+  private
+
+  def booking_payments_params
+    params.permit(
+      :stripeToken,
+      :property_id,
+      :user_id,
+      :checkin_date,
+      :checkout_date,
+      :base_fare,
+      :service_fee,
+      :total_fare
+     )
   end
 end
