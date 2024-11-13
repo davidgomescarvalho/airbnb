0802407065dff4605d440484e3886bd6b67eaaed
david gomes <davidgomesk8@gmail.com>
2024-11-13 21:50:34 +0100

    fixing total payment is nil

--- app/controllers/booking_payments_controller.rb
+++ app/controllers/booking_payments_controller.rb
@@ -1,6 +1,28 @@
 class BookingPaymentsController < ApplicationController
   def create
+
+    @property = Property.find(booking_payments_params[:property_id])
     raise
+    stripe_price = Stripe::Price.create({
+      currency: 'usd',
+      unit_amount: Money.from_amount(BigDecimal(booking_payments_params[:total_amount])).cents,
+      product_data: {
+        name: @property.name
+      }
+    })
+
+    stripe_session = Stripe::Checkout::Session.create({
+      success_url: 'https://example.com/success',
+      line_items: [
+        {
+          price: stripe_price.id,
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+    })
+
+    redirect_to stripe_session.url, allow_other_host: true, status: 303
   end
 
   private
@@ -18,8 +40,4 @@ class BookingPaymentsController < ApplicationController
      )
   end
 
-  def property
-    @property = Property.find(params[:property_id])
-  end
-  
 end
