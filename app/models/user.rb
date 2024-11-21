3e37dab268a4e441a1c5a7e3efe76e6c99912443
david gomes <davidgomesk8@gmail.com>
2024-11-21 19:42:02 +0100

    dependent destroy in users profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :profile
+  has_one :profile, dependent: :destroy
 
   has_many :wishlists, dependent: :destroy
   has_many :wishlisted_properties, through: :wishlists, source: :property, dependent: :destroy
