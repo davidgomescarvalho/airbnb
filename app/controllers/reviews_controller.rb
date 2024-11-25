6be1db90813496bdda2df29f3c7102746bab0779
david gomes <davidgomesk8@gmail.com>
2024-11-25 21:19:52 +0100

    added reviews_params in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,5 +4,24 @@ class ReviewsController < ApplicationController
     @reservation = Reservation.find(params[:reservation_id])
   end
 
+  def create
+
+  end
+
   private
+
+  def reviews_params
+    params.permit(
+      :user_id,
+      :property_id,
+      :reservation_id,
+      :content,
+      :cleanliness_rating,
+      :accuracy_rating,
+      :checkin_rating,
+      :communication_rating,
+      :location_rating,
+      :value_rating
+    )
+  end
 end
