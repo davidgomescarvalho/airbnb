71ec8d2ce9dff0ef252a65bd98e4ccefed843e62
david gomes <davidgomesk8@gmail.com>
2024-11-28 20:52:48 +0100

    added target in controller for each star

--- app/javascript/controllers/rating_controller.js
+++ app/javascript/controllers/rating_controller.js
@@ -1,3 +1,9 @@
 import { Controller } from "@hotwired/stimulus";
 
-export default class extends Controller {}
+export default class extends Controller {
+  setRating(e) {
+    console.log(e.currentTarget.tagName);
+
+    const starElement = e.currentTarget;
+  }
+}
