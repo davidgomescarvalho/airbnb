2474ddbfd15fd31d117b93ac30c79007533edb94
david gomes <davidgomesk8@gmail.com>
2024-12-02 19:38:33 +0100

    successfully selected the valur for the input selected for star in a review

--- app/javascript/controllers/rating_controller.js
+++ app/javascript/controllers/rating_controller.js
@@ -3,6 +3,11 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   setRating(e) {
     const rating = e.currentTarget.dataset.rating;
-    console.log("Rating: ", rating);
+
+    const ratingInput = e.currentTarget.parentNode.querySelector(
+      'input[data-rating-target="cleanliness_rating"]'
+    );
+
+    ratingInput.value = rating;
   }
 }
