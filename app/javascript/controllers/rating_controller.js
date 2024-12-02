5d2d44ad2213c53895fafd5e7d30e903c6e70aba
david gomes <davidgomesk8@gmail.com>
2024-12-02 19:52:30 +0100

    updated controller

--- app/javascript/controllers/rating_controller.js
+++ app/javascript/controllers/rating_controller.js
@@ -4,10 +4,11 @@ export default class extends Controller {
   setRating(e) {
     const rating = e.currentTarget.dataset.rating;
     const ratingCategory = e.currentTarget.dataset.category;
-
-    const ratingInput = e.currentTarget.parentNode.querySelector(
+    const rootElement = e.currentTarget.parentNode;
+    const ratingInput = rootElement.querySelector(
       `input[data-rating-target="${ratingCategory}_rating"]`
     );
+
     if (ratingInput) {
       ratingInput.value = rating;
     }
