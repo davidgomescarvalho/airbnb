5c41815e2fc1b9ecfbd36e1bddee2c491070c0d8
david gomes <davidgomesk8@gmail.com>
2024-12-10 17:51:45 +0100

    added default value to reviews count and runned migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_09_140549) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_10_164151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,7 +99,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_09_140549) do
     t.datetime "updated_at", null: false
     t.integer "price_cents"
     t.string "price_currency"
-    t.integer "reviews_count"
+    t.integer "reviews_count", default: 0
     t.decimal "average_final_rating", default: "0.0"
     t.integer "guest_count", default: 0
     t.integer "bedroom_count", default: 0
