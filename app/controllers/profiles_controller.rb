7c5fba11be2f0e3b99e5c9135018e06f447a5be0
david gomes <davidgomesk8@gmail.com>
2025-01-10 15:22:55 +0100

    before action to set_profile and added params in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,27 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
-  def edit
+  before_action :set_profile, only: [:edit, :update]
+
+
+  def update
+     @profile.update!(profile_params)
+  end
+
+  private
+
+  def set_profile
     @profile = current_user.profile
   end
 
-  def update
-    raise
+  def profile_params
+    params.require(:profile).permit(
+      :name,
+      :address_1,
+      :address_2,
+      :city,
+      :state,
+      :country
+    )
   end
+
 end
