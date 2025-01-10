6509f3b5e063765fbbe0fcd1247e29c3d3d816e8
david gomes <davidgomesk8@gmail.com>
2025-01-10 15:27:33 +0100

    finished to update profiles controller, tested correctly

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,11 @@ class ProfilesController < ApplicationController
 
 
   def update
-     @profile.update!(profile_params)
+    if @profile.update!(profile_params)
+      redirect_to edit_profile_path, notice: "Profile updated successfully"
+    else
+      redirect_back fallback_location: edit_profile_path, alert: "Profile could not be updated"
+    end
   end
 
   private
