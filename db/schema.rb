46a8d7cf1d8ffc45e017975599954a32775c9adc
david gomes <davidgomesk8@gmail.com>
2025-01-20 22:23:50 +0100

    shcema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_16_173035) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_20_195418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,7 +94,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_16_173035) do
     t.string "address_2"
     t.string "city"
     t.string "state"
-    t.string "country"
+    t.string "country_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents"
