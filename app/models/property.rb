3811eec61003a88c2eba09c69aa2232500005748
david gomes <davidgomesk8@gmail.com>
2025-01-20 22:29:03 +0100

    fixed property model error

--- app/models/property.rb
+++ app/models/property.rb
@@ -6,7 +6,7 @@ class Property < ApplicationRecord
   validates :address_1, presence: true
   validates :city, presence: true
   validates :state, presence: true
-  validates :_code, presence: true
+  validates :country_code, presence: true
 
   monetize :price_cents, allow_nil: true
 
