1006c942a6d1b93ed12351ef04c25825e9a0526e
david gomes <davidgomesk8@gmail.com>
2025-01-23 20:43:31 +0100

    added methods and params

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,3 +1,17 @@
 class EmailsController < ApplicationController
+  before_action :authenticate_user!
 
+  def update
+    raise
+    # if @profile.update!(profile_params)
+    #   redirect_to edit_profile_path, notice: "Profile updated successfully"
+    # else
+    #   redirect_back fallback_location: edit_profile_path, alert: "Profile could not be updated"
+    # end
+  end
+
+  private
+
+  def user_params
+  end
 end
