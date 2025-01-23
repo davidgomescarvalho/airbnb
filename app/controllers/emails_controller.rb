482a612e19be3ef7864708dddf7858bf3531f51f
david gomes <davidgomesk8@gmail.com>
2025-01-23 21:05:40 +0100

    deleted password in emails controller to create a new mvc

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,17 +2,16 @@ class EmailsController < ApplicationController
   before_action :authenticate_user!
 
   def update
-    raise
     if current_user.update!(profile_params)
-      redirect_to edit_profile_path, notice: "Profile updated successfully"
+      redirect_to edit_profile_path, notice: "Email updated successfully"
     else
-      redirect_back fallback_location: edit_profile_path, alert: "Profile could not be updated"
+      redirect_back fallback_location: edit_profile_path, alert: "Email could not be updated"
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email)
   end
 end
