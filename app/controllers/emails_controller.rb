16869deb220b4b8bc42b33ca396e6315089ad5d1
david gomes <davidgomesk8@gmail.com>
2025-01-23 21:02:52 +0100

    added params in emails controller

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,15 +3,16 @@ class EmailsController < ApplicationController
 
   def update
     raise
-    # if @profile.update!(profile_params)
-    #   redirect_to edit_profile_path, notice: "Profile updated successfully"
-    # else
-    #   redirect_back fallback_location: edit_profile_path, alert: "Profile could not be updated"
-    # end
+    if current_user.update!(profile_params)
+      redirect_to edit_profile_path, notice: "Profile updated successfully"
+    else
+      redirect_back fallback_location: edit_profile_path, alert: "Profile could not be updated"
+    end
   end
 
   private
 
   def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
