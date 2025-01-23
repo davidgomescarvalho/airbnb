8c3c5fa7627d48a20719f107cf6465eac554a97b
david gomes <davidgomesk8@gmail.com>
2025-01-23 21:06:43 +0100

    fixed update method

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,7 +2,7 @@ class EmailsController < ApplicationController
   before_action :authenticate_user!
 
   def update
-    if current_user.update!(profile_params)
+    if current_user.update!(user_params)
       redirect_to edit_profile_path, notice: "Email updated successfully"
     else
       redirect_back fallback_location: edit_profile_path, alert: "Email could not be updated"
