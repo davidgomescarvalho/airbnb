ddb699b51eceb550669f1d319352afb6e0f04b57
david gomes <davidgomesk8@gmail.com>
2025-01-27 17:24:59 +0100

    added reservations to owner dashboard and some fixes

---
+++ app/controllers/owner/reservations_controller.rb
@@ -0,0 +1,9 @@
+module Owner
+  class ReservationsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+      @reservations = current_user.reservations
+    end
+  end
+end
