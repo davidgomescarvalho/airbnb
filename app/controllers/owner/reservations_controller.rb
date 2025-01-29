baf5d3f99945611784975f67d55616dacc4a1ce1
david gomes <davidgomesk8@gmail.com>
2025-01-29 12:10:13 +0100

    map flatten case where I only show my reservation and not in my dashboard

--- app/controllers/owner/reservations_controller.rb
+++ app/controllers/owner/reservations_controller.rb
@@ -3,7 +3,7 @@ module Owner
     before_action :authenticate_user!
 
     def index
-      @reservations = current_user.reservations
+      @reservations = current_user.properties.map(&:reservations).flatten
     end
   end
 end
