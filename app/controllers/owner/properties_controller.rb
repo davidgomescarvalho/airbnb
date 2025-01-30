3538cee5d435f78a1d540780d37e127ca49c5c2f
david gomes <davidgomesk8@gmail.com>
2025-01-30 21:16:34 +0100

    finished and tested edit property

--- app/controllers/owner/properties_controller.rb
+++ app/controllers/owner/properties_controller.rb
@@ -8,13 +8,31 @@ module Owner
     end
 
     def update
-
+      if @property.update!(property_params)
+        redirect_to edit_owner_property_path, notice: 'Property was successfully updated.'
+      else
+        redirect_back fallback_location: edit_owner_property_path, alert: 'Property could not be updated.'
+      end
     end
 
     private
 
     def set_property
       @property = current_user.properties.find(params[:id])
+    end
+
+    def property_params
+      params.require(:property).permit(
+        :name,
+        :price,
+        :headline,
+        :description,
+        :address_1,
+        :address_2,
+        :city,
+        :state,
+        :country_code
+      )
     end
   end
 end
