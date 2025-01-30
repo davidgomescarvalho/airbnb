42738405f8d828ce786d4ca034dbe52507f7baf8
david gomes <davidgomesk8@gmail.com>
2025-01-30 20:40:41 +0100

    added edit template to owner property as well as update method in controller and fixed edit html

--- app/controllers/owner/properties_controller.rb
+++ app/controllers/owner/properties_controller.rb
@@ -1,9 +1,20 @@
 module Owner
   class PropertiesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_property, only: [:edit, :update]
 
     def index
       @properties = current_user.properties
+    end
+
+    def update
+
+    end
+
+    private
+
+    def set_property
+      @property = current_user.properties.find(params[:id])
     end
   end
 end
