54e814552f37f14367fa1075427840ee60fd70cb
david gomes <davidgomesk8@gmail.com>
2025-01-31 18:46:11 +0100

    amenities updtes in controller and routes successfull

--- app/controllers/owner/properties_controller.rb
+++ app/controllers/owner/properties_controller.rb
@@ -1,7 +1,7 @@
 module Owner
   class PropertiesController < ApplicationController
     before_action :authenticate_user!
-    before_action :set_property, only: [:edit, :update]
+    before_action :set_property, only: [:edit, :update, :update_amenities]
 
     def index
       @properties = current_user.properties
@@ -15,10 +15,22 @@ module Owner
       end
     end
 
+    def update_amenities
+      if @property.update!(amenities_params)
+        redirect_to edit_owner_property_path, notice: 'Amenities was successfully updated.'
+      else
+        redirect_back fallback_location: edit_owner_property_path, alert: 'Amenities could not be updated.'
+      end
+    end
+
     private
 
     def set_property
       @property = current_user.properties.find(params[:id])
+    end
+
+    def amenities_params
+      params.require(:property).permit(:amenity_ids => [])
     end
 
     def property_params
