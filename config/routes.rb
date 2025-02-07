5785354d7a60b5864321288761c16efa321e0c53
david gomes <davidgomesk8@gmail.com>
2025-02-07 14:39:41 +0100

    successfully adding pictures in edit property

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ Rails.application.routes.draw do
     resources :properties do
       member do
         patch 'update_amenities'
+        patch 'add_images'
         delete 'remove_image'
       end
     end
