8542f9682c055faffa8e1ae833eae2c9e7c2faae
david gomes <davidgomesk8@gmail.com>
2025-02-18 18:55:32 +0100

    added search_params and emptyparams method in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,19 @@
 class SearchController < ApplicationController
   def index
-
+    if are_all_params_empty?
+      redirect_to root_path
+      return
+    end
   end
+
+  private
+
+  def are_all_params_empty?
+    !search_params[:country_code].present? && !search_params[:checkin_date].present? && !search_params[:checkout_date].present?
+  end
+
+  def search_params
+    params.permit(:country_code, :checkin_date, :checkout_date)
+  end
+
 end
